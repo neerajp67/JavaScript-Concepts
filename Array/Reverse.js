2f6d00be39ca67aeb79266432c485e39c4df14eb
Neeraj Pandey <neerajpandey025@gmai.com>
2025-06-01 17:01:31 +0530

    Added 1st file, array reverse

---
+++ Array/Reverse.js
@@ -0,0 +1,14 @@
+// this file will contain the solution for reversing an array in different ways:
+
+//Define common array for all the solutions
+const arr = [1, 2, 3, 4, 5, 6, 7];
+let n = arr.length;
+
+// method 1: using Array.reverse(), changes orignal array
+let reversedArray = arr.reverse();
+console.log(reversedArray);
+
+
+
+
+
