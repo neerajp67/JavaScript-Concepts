836e86c5651b7bab88d0fb2b0f7ade548b934f9c
Neeraj Pandey <neerajpandey025@gmai.com>
2025-06-24 21:09:46 +0530

    code comment

--- Array/ItemOccurrence.js
+++ Array/ItemOccurrence.js
@@ -6,15 +6,15 @@ const n = arr.length;
 //first recurring item in an array
 
 // method 1: using for loop to iterate through the array and return 1st recurring item
-function firstRecurringItem(arr) {
-    for (let i = 0; i < n; i++) {
-        for (let j = i + 1; j < n; j++) {
-            if (arr[i] === arr[j]) {
-                return arr[i];
-            }
-        }
-    }
-    return null; // if no recurring item found
-}
-console.log(firstRecurringItem(arr));
+// function firstRecurringItem(arr) {
+//     for (let i = 0; i < n; i++) {
+//         for (let j = i + 1; j < n; j++) {
+//             if (arr[i] === arr[j]) {
+//                 return arr[i];
+//             }
+//         }
+//     }
+//     return null; // if no recurring item found
+// }
+// console.log(firstRecurringItem(arr));
 
