e48496fe740e4560e65aab3b14e2a6a212836ffd
Neeraj Pandey <neerajpandey025@gmai.com>
2025-06-25 22:54:00 +0530

    code comment

--- Array/ItemOccurrence.js
+++ Array/ItemOccurrence.js
@@ -22,19 +22,19 @@ const n = arr.length;
 // first non-recurring item in an array
 
 // method 1: using for loop to iterate through the array and return first non-recurring item 
-function firstNonRecurringItem() {
-    for (let i = 0; i < n - 1; i++) {
-        for (let j = i + 1; j < n; j++) {
-            if (arr[i] === arr[j]) {
-                break; // if we find a match, break the inner loop
-            }
-            if (j === n - 1) { // if we reach the end of the array without finding a match
-                return arr[i]; // return the non-recurring item
-            }
-        }
-    }
-}
-console.log(firstNonRecurringItem());
+// function firstNonRecurringItem() {
+//     for (let i = 0; i < n - 1; i++) {
+//         for (let j = i + 1; j < n; j++) {
+//             if (arr[i] === arr[j]) {
+//                 break; // if we find a match, break the inner loop
+//             }
+//             if (j === n - 1) { // if we reach the end of the array without finding a match
+//                 return arr[i]; // return the non-recurring item
+//             }
+//         }
+//     }
+// }
+// console.log(firstNonRecurringItem());
 
 
 
