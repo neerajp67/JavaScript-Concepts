ba4f5c7b3e8ba503e2844ea2840905c49c67295e
Neeraj Pandey <neerajpandey025@gmai.com>
2025-08-09 22:50:39 +0530

    Leetcode 231

---
+++ JavaScript Practice/powerOfTwo.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function (n) {
+    if (n === 1) return true
+    if (n % 2 !== 0 || n < 1) return false;
+    return isPowerOfTwo(n / 2)
+};
+
+const n = 16;
+console.log(isPowerOfTwo(n));
