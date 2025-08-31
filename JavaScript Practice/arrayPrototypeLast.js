859fe37c9eebdd51cab5358e6993127a1447fdce
Neeraj Pandey <neerajpandey025@gmai.com>
2025-08-31 21:08:54 +0530

    Leetcode 2619

---
+++ JavaScript Practice/arrayPrototypeLast.js
@@ -0,0 +1,10 @@
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function() {
+    if(!this.length) return;
+    return this[this.length -1];
+};
+
+  const arr = [1, 2, 3];
+  console.log(arr.last());
